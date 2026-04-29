ef8ef6f4aab4dd1cbe65d822de3bc4715fd8dd07
Hitanshi29 <hitanshirana2912@gmail.com>
2026-04-29 16:31:54 +0530

    Add new website module

---
+++ website_practice_module/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo Module Developed by Bizople Solutions Pvt. Ltd.
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Practice Website',
+    'version': '1.0',
+    'author': 'Bizople Solutions Pvt. Ltd.',
+    'website': 'https://www.bizople.com',
+    'summary': '',
+    'depends': ['crm','website'],
+    'data': [
+    ],
+    'sequence' : 1,
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+
+}       
